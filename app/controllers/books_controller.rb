e900ffe551870f378683fe5ae04ebef42892865f
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-07 14:40:14 +0300

    Generated controller methods for books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,23 @@
 class BooksController < ApplicationController
+    
+    def index
+    end
+
+    def show
+    end
+
+    def create
+    end
+
+    def update
+    end
+
+    def delete
+    end 
+
+
+    private
+
+    def BooksParams
+        params.permit(:title, :description, :author, :published)
 end
