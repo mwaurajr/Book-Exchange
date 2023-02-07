acf57e880737c4ae674dd628c131268cea6fbd19
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-07 15:58:38 +0300

    Added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
     has_many :books
 
     attribute :phoneNumber, :integer, limit: 13
+
+    validates :name, :address, presence: true
+   
+    validates :email, 
+    uniqueness: true, 
+    length: { in: 3..255 }, 
+    format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone_number,
+    format: { with: /\A\d{10}\z/ },
+    length: { maximum: 10 },
+    allow_nil: false
+    
 end
