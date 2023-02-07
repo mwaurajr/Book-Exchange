93f0dc6427eb6d28e35cfb34bf696b5a3f48f654
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-07 19:14:46 +0300

    Added association to bookexchange

--- app/models/book_exchange.rb
+++ app/models/book_exchange.rb
@@ -1,6 +1,7 @@
 class BookExchange < ApplicationRecord
 
-    belongs_to :books
-    belongs_to :users
+    belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
+    belongs_to :receiver, class_name: 'User', foreign_key: 'receiver_id'
+    belongs_to :book
     
 end
