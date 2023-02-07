ea897c9256cc4bb5512c6875b50335b6c9dc3ddd
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-07 14:31:29 +0300

    Generated the correct books resource

---
+++ db/migrate/20230207113041_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[7.0]
+  def change
+    create_table :books do |t|
+      t.integer :user_id
+      t.string :title
+      t.string :author
+      t.string :description
+
+      t.timestamps
+    end
+    add_index :books, :user_id
+  end
+end
