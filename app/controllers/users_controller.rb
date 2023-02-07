d063e4ffb2f758b2ebb2387c50c0b03776cd4d6d
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-07 14:50:44 +0300

    finalizing users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,37 @@
 class UsersController < ApplicationController
 
-    def index
-    end
-
     def show
-    end
-
-    def create
-    end
-
-    def update
-    end
-
-    def delete
-    end 
+        @user = User.find(user_params[:id])
+        render :show
+      end
+    
+      def create 
+        @user = User.create(user_params)
+        if @user.save 
+          render :show
+        else 
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      def update
+        @user = User.find(current_user.id)
+        if @user.update(user_params)
+          @user.save
+          render :show
+        else
+          render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+        end
+      end
+    
+      def index 
+        @users = User.all
+        render :index
+      end
 
     private
 
-    def UsersParams
-        params.permit(:name, :email, :password, :phoneNumber, :address,:bio, :profile_pic)
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :phoneNumber, :address,:bio, :profile_pic)
     end
 end
