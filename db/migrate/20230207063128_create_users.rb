c29de7bca1bf2b0ce93b0ed46e678bd99d570edd
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-07 09:31:47 +0300

    Created user resource

---
+++ db/migrate/20230207063128_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.string :password_digest
+      t.text :bio
+      t.string :address
+      t.integer :phoneNumber
+      t.string :profile_pic
+
+      t.timestamps
+    end
+  end
+end
