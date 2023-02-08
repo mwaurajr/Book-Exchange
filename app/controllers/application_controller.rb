037dc8b0dff6e01574354bc3cb3c9d7a7a8d749c
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-08 19:42:21 +0300

    Added Jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::API
-    
+    private
+
+  def current_user
+    @current_user ||= User.find_by(id: jwt_user_id)
+  end
+
+  def jwt_user_id
+    decoded_token&.first&.fetch("user_id")
+  end
+
+  def decoded_token
+    JWT.decode(
+      jwt_token,
+      Rails.application.credentials.secret_key_base,
+      true,
+      { algorithm: "HS256" }
+    )
+  rescue JWT::DecodeError
+    nil
+  end
+
+  def jwt_token
+    request.headers["Authorization"]&.split(" ")&.last || cookies["jwt_token"]
+  end
 end
