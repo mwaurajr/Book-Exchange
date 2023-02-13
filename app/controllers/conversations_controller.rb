a063b619805cd1677543cc7137ff3456a1618ace
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-13 14:48:04 +0300

    delete a conv

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,2 +1,9 @@
 class ConversationsController < ApplicationController
+
+    def destroy
+        conversation = Conversation.find(params[:id])
+        conversation.messages.destroy_all
+        conversation.destroy
+        head :ok
+    end
 end
