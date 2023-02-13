3515e33c51ddebec710e28504075397a5548527c
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-13 14:34:23 +0300

    generate cov

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,2 +1,6 @@
 class Conversation < ApplicationRecord
+    def self.get(sender, recipient)
+        conversation = Conversation.where("(sender_id = ? AND recipient_id = ?) OR (sender_id = ? AND recipient_id = ?)", sender.id, recipient.id, recipient.id, sender.id).first
+        conversation ||= Conversation.create(sender: sender, recipient: recipient)
+      end
 end
