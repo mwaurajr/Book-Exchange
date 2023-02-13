e09a00cba57d31da5ddea1b428f922862a0e067a
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-13 14:37:23 +0300

    Debbuged

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :messages
+  # resources :messages
   resources :reviews
   resources :book_exchanges
   resources :books
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
 post "/signup", to: "users#create"
 post "/login", to: "sessions#create"
 get "/authorized", to: "sessions#show"
+
 end
