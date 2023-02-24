3767e3960fd7dfb88caa13a86e9480b35ddc1701
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-24 16:39:50 +0300

    removed reviews

--- app/models/review.rb
+++
@@ -1,7 +0,0 @@
-class Review < ApplicationRecord
-  belongs_to :book_exchange
-  belongs_to :user
-
-  validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
-  validates :content, presence: true, length: { maximum: 500 }
-end
