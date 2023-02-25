799fe6238c817ebb0ca30740d195ced72c376bff
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-25 11:07:57 +0300

    deleted conversation resource

--- app/controllers/conversations_controller.rb
+++
@@ -1,9 +0,0 @@
-class ConversationsController < ApplicationController
-
-    def destroy
-        conversation = Conversation.find(params[:id])
-        conversation.messages.destroy_all
-        conversation.destroy
-        head :ok
-    end
-end
--- db/migrate/20230213112418_create_conversations.rb
+++
@@ -1,8 +0,0 @@
-class CreateConversations < ActiveRecord::Migration[7.0]
-  def change
-    create_table :conversations do |t|
-
-      t.timestamps
-    end
-  end
-end
