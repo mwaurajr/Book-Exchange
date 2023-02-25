570258b2f08aed8041de627dbd2064d03cfa911e
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-25 11:14:05 +0300

    added user and conv association

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,5 +23,9 @@ class User < ApplicationRecord
     has_many :sent_book_exchanges, foreign_key: 'sender_id', class_name: 'BookExchange'
     has_many :received_book_exchanges, foreign_key: 'receiver_id', class_name: 'BookExchange'
     has_many :reviews, dependent: :destroy
+
+    has_many :sent_conversations, class_name: 'Conversation', foreign_key: 'sender_id'
+    has_many :received_conversations, class_name: 'Conversation', foreign_key: 'recipient_id'
+    has_many :messages
     
 end
