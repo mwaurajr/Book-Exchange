62b9395b6e4ff95b1fb02c3bd41f84b1c6504dc8
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-25 11:18:37 +0300

    reran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_24_134002) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_25_081609) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,8 +66,22 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_134002) do
   end
 
   create_table "conversations", force: :cascade do |t|
+    t.integer "recipient_id"
+    t.integer "sender_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "conversation_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "read"
+    t.boolean "delivered"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -98,6 +112,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_24_134002) do
   add_foreign_key "book_exchanges", "books"
   add_foreign_key "book_exchanges", "users", column: "receiver_id"
   add_foreign_key "book_exchanges", "users", column: "sender_id"
+  add_foreign_key "messages", "conversations"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "users"
 end
