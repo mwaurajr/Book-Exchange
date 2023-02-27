7bc9198df8da41358a5969b5f198e6fa04ccb94c
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-27 09:33:29 +0300

    added actions to the messages channel

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,9 +1,9 @@
 class MessagesChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "messages_channel"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    ActionCable.server.broadcast "chat_channel", message: "#{current_user.username} has left the chat"
   end
 end
