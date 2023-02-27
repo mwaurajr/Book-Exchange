1dab65d605f2338ca8a419fed128a3ba656a1347
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-27 14:41:04 +0300

    updated books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,45 @@
 class BooksController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
+  before_action :require_login
 
   def index
     render json: current_user.books
   end
 
     def show
+      book = Book.find(params[:id])
+      if book 
+        render json: book, status: :ok
+      else 
+        render json: { error: "Book not found" }, status: :not_found
     end
 
     def create(books_params)
+      book = Book.create!(books_params)
+      if book
+        render json: book, status: :created
+      else
+        render json: {erro: "Invalid Book"}, status: :unprocessable_entity
+
     end
 
     def update(books_params)
+      book = Book.find(params[:id])
+      render json: book.update!(book_params), status: :updated
+
     end
 
     def delete
+      book = Book.find(params[:id])
+      book.destroy
+      head :no_content
     end 
 
 
     private
 
     def books_params
-        params.require(:books).permit(:title, :description, :author, :published)
+        params.require(:books).permit(:title, :description, :author, :published, :condition)
     end
 
     def require_login
