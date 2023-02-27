55af3280a0c9c4700cea82305ed8ed22f539cadd
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-02-27 10:31:23 +0300

    autheniticate user first

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,11 +1,26 @@
 class MessagesChannel < ApplicationCable::Channel
-  before_action :authenticate_user!
-  
   def subscribed
-    stream_from "messages_#{params[:conversation_id]}"
+    conversation = Conversation.find(params[:conversation_id])
+    stream_for conversation
+  end
+
+  def receive(data)
+    message = current_user.messages.build(data)
+    message.conversation_id = params[:conversation_id]
+    message.save!
   end
 
   def unsubscribed
-    ActionCable.server.broadcast "chat_channel", message: "#{current_user.username} has left the chat"
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  private
+
+  def authenticate_user!
+    token, _options = token_and_options(request)
+    user_id = AuthenticationTokenService.decode(token)
+    @current_user = User.find(user_id)
+  rescue ActiveRecord::RecordNotFound, JWT::DecodeError
+    reject_unauthorized_connection
   end
 end
