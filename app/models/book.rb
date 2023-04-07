13c6eaed8e559769457bb955aa156980f978ec05
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-07 19:52:49 +0300

    added all association

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,4 +5,14 @@ class Book < ApplicationRecord
 
     has_many :book_exchanges
     has_many :reviews, dependent: :destroy
+
+    def create_notification
+        Notification.create(user_id: user_id, message: "A new book has been posted!", url: "/books/#{id}", read: false)
+      end
+    
+    
+   
+     
+    
+     
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,2 +1,5 @@
 class Notification < ApplicationRecord
+    def create_notification
+        Notification.create(user_id: user_id, message: "A new book has been posted!", url: "/books/#{id}", read: false)
+      end
 end
