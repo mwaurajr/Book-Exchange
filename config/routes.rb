c3093e775d8d7a3f78c1aaec7608a1fffea2cad2
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-07 19:54:25 +0300

    added notification route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :notifications, only: [:index]
 
   # Defines the root path route ("/")
   # root "articles#index"
