36f66a1d8b147d59e92be601567a0904baf31481
Frankiline <frankiline.mwaura@student.moringaschool.com>
2023-04-07 19:48:13 +0300

    created a notification table and migrated

---
+++ db/migrate/20230407164709_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[7.0]
+  def change
+    create_table :notifications do |t|
+      t.integer :user_id
+      t.string :message
+      t.string :url
+      t.boolean :read
+
+      t.timestamps
+    end
+  end
+end
